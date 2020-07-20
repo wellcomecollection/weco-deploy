ad07e3e64c4d7db2a1800b3fa5a2327db5104936
Alex Chan <alex@alexwlchan.net>
2020-07-20 13:12:26 +0100

    Add a method for pretty-printing dates

--- src/deploy/pretty_printing.py
+++ src/deploy/pretty_printing.py
@@ -1,3 +1,5 @@
+import datetime
+
 
 def pprint_nested_tree(tree, indent=0):
     lines = []
@@ -65,3 +67,26 @@ def build_tree_from_paths(paths):
 def pprint_path_keyval_dict(paths):
     tree = build_tree_from_paths(paths)
     return pprint_nested_tree(tree)
+
+
+
+def pprint_date(date_obj, *, now=datetime.datetime.now()):
+    assert date_obj <= now
+
+    difference = (now - date_obj)
+
+    is_today = date_obj.date() == now.date()
+    is_yesterday = date_obj.date() == (now - datetime.timedelta(days=1)).date()
+
+    if difference.total_seconds() <= 120:
+        return "just now"
+    elif is_today and difference.total_seconds() <= 60 * 60:
+        return date_obj.strftime("today @ %H:%M") + " (%d min ago)" % (difference.total_seconds() // 60)
+    elif is_today:
+        return date_obj.strftime("today @ %H:%M")
+    elif is_yesterday and difference.total_seconds() <= 60 * 60:
+        return date_obj.strftime("yesterday @ %H:%M") + " (%d min ago)" % (difference.total_seconds() // 60)
+    elif is_yesterday:
+        return date_obj.strftime("yesterday @ %H:%M")
+    else:
+        return date_obj.strftime("%Y-%m-%d @ %H:%M")
