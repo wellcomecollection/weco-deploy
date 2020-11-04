1d7d138602d15b60d55a75cd309331ca8f930306
Alex Chan <a.chan@wellcome.org>
2020-11-04 15:22:28 +0000

    Test initialisation in the release store
    Notably: we need to use @moto.dynamodb2, not @moto.dynamodb.  This tripped
    me up last time!
    
    See https://github.com/spulec/moto/issues/3087

--- src/deploy/release_store.py
+++ src/deploy/release_store.py
@@ -1,11 +1,23 @@
 import abc
 
+from botocore.exceptions import ClientError
+
 from . import iam
 
 
 class ReleaseStore(abc.ABC):
     @abc.abstractmethod
     def describe_initialisation(self):
+        """
+        Returns a human-readable string to describe the initialisation process.
+        """
+        pass
+
+    @abc.abstractmethod
+    def initialise(self):
+        """
+        Do any initialisation steps to make the store ready for use.
+        """
         pass
 
 
@@ -16,18 +28,108 @@ class MemoryReleaseStore(ReleaseStore):
     def describe_initialisation(self):
         return "Create in-memory release store"
 
+    def initialise(self):
+        pass
+
 
 class DynamoReleaseStore(ReleaseStore):
     def __init__(self, project_id, region_name, role_arn):
         self.project_id = project_id
-        self.table_name = f"wellcome-releases-{project_id}"
         session = iam.get_session(
             session_name="ReleaseToolDynamoDbReleaseStore",
             role_arn=role_arn,
             region_name=region_name
         )
-        self.dynamo_db = session.resource("dynamodb")
-        self.table = self.dynamo_db.Table(self.table_name)
+        self.dynamodb = session.resource("dynamodb")
+        self.table = self.dynamodb.Table(f"wellcome-releases-{project_id}")
+
+    @property
+    def table_name(self):
+        return self.table.name
 
     def describe_initialisation(self):
         return f"Create DynamoDB table {self.table_name}"
+
+    def initialise(self):
+        # Attempt to load the description of the table from DynamoDB.  If this
+        # fails, we know the table doesn't exist yet and we should try to
+        # create it.
+        try:
+            self.table.load()
+        except self.dynamodb.meta.client.exceptions.ResourceNotFoundException:
+            self._create_table()
+
+    def _create_table(self):
+        self.dynamodb.create_table(
+            TableName=self.table_name,
+            KeySchema=[
+                {
+                    'AttributeName': 'release_id',
+                    'KeyType': 'HASH'
+                }
+            ],
+            AttributeDefinitions=[
+                {
+                    'AttributeName': 'release_id',
+                    'AttributeType': 'S'
+                },
+                {
+                    'AttributeName': 'project_id',
+                    'AttributeType': 'S'
+                },
+                {
+                    'AttributeName': 'date_created',
+                    'AttributeType': 'S'
+                },
+                {
+                    'AttributeName': 'last_date_deployed',
+                    'AttributeType': 'S'
+                }
+            ],
+            GlobalSecondaryIndexes=[
+                {
+                    'IndexName': 'project_gsi',
+                    'KeySchema': [
+                        {
+                            'AttributeName': 'project_id',
+                            'KeyType': 'HASH'
+                        },
+                        {
+                            'AttributeName': 'date_created',
+                            'KeyType': 'RANGE'
+                        },
+                    ],
+                    'Projection': {
+                        'ProjectionType': 'ALL'
+                    },
+                    'ProvisionedThroughput': {
+                        'ReadCapacityUnits': 1,
+                        'WriteCapacityUnits': 1
+                    }
+                },
+                {
+                    'IndexName': 'deployment_gsi',
+                    'KeySchema': [
+                        {
+                            'AttributeName': 'project_id',
+                            'KeyType': 'HASH'
+                        },
+                        {
+                            'AttributeName': 'last_date_deployed',
+                            'KeyType': 'RANGE'
+                        },
+                    ],
+                    'Projection': {
+                        'ProjectionType': 'ALL'
+                    },
+                    'ProvisionedThroughput': {
+                        'ReadCapacityUnits': 1,
+                        'WriteCapacityUnits': 1
+                    }
+                }
+            ],
+            ProvisionedThroughput={
+                'ReadCapacityUnits': 1,
+                'WriteCapacityUnits': 1
+            }
+        )
--- tests/test_release_store.py
+++ tests/test_release_store.py
@@ -1,35 +1,55 @@
 import abc
+import contextlib
 import secrets
 
+from botocore.exceptions import ParamValidationError
 import moto
+import pytest
 
 from deploy.release_store import DynamoReleaseStore, MemoryReleaseStore
 
 
 class ReleaseStoreTestsMixin:
     @abc.abstractmethod
+    @contextlib.contextmanager
     def create_release_store(self):
         pass
 
     def test_description(self):
-        release_store = self.create_release_store()
-        description = release_store.describe_initialisation()
-        assert isinstance(description, str)
-        assert len(description) > 0
+        with self.create_release_store() as release_store:
+            description = release_store.describe_initialisation()
+            assert isinstance(description, str)
+            assert len(description) > 0
+
+    def test_can_initialise(self):
+        with self.create_release_store() as release_store:
+            release_store.initialise()
+
+            # Calling initialise() a second time should have no effect
+            release_store.initialise()
 
 
 class TestMemoryReleaseStore(ReleaseStoreTestsMixin):
+    @contextlib.contextmanager
     def create_release_store(self):
-        return MemoryReleaseStore()
+        yield MemoryReleaseStore()
 
 
 class TestDynamoReleaseStore(ReleaseStoreTestsMixin):
-    @moto.mock_iam()
-    @moto.mock_sts()
-    @moto.mock_dynamodb()
+    @contextlib.contextmanager
     def create_release_store(self):
-        return DynamoReleaseStore(
-            project_id=f"test_project-{secrets.token_hex()}",
-            region_name="eu-west-1",
-            role_arn="arn:aws:iam::0123456789:role/example_role"
-        )
+        with moto.mock_dynamodb2(), moto.mock_sts(), moto.mock_iam():
+            yield DynamoReleaseStore(
+                project_id=f"test_project-{secrets.token_hex()}",
+                region_name="eu-west-1",
+                role_arn="arn:aws:iam::0123456789:role/example_role"
+            )
+
+    def test_unexpected_error_at_initialisation_is_raised(self):
+        with self.create_release_store() as release_store:
+            release_store.table = release_store.dynamodb.Table("")
+
+            # DynamoDB tables should have non-empty length.  Trying to initialise
+            # a table whose name is the empty string will fail.
+            with pytest.raises(ParamValidationError):
+                release_store.initialise()
